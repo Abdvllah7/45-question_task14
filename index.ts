a8d8941cc7d2c8c181039861bf5227649a5afa34
Abdvllah7 <167926753+Abdvllah7@users.noreply.github.com>
2024-05-14 05:32:58 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,16 @@
+let guestlist: Array<string> = [
+    "Zia Khan",
+    "Jahanzain Tayab",
+    "Abid Shirwani",
+    "Muhammad Ali Jinnah",
+    "Albert Einstein",
+    "Muhammad Ali"
+];
+
+//loop for each to print invitation
+
+guestlist.forEach((guestName) =>
+{
+console.log(`Dear ${guestName}, you are invired to dinner please join us.`)
+
+})
